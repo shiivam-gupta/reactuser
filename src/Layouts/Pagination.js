741566c71a024493a3cf229980ab52fb8bbd3859
najmussaher <najmussaher.siddiqui@appinventiv.com>
2021-04-07 18:03:45 +0530

    changes for paination

--- src/Layouts/Pagination.js
+++ src/Layouts/Pagination.js
@@ -1,79 +1,82 @@
-import { React,useEffect,useState } from "react";
+import { React, useEffect, useState } from "react";
 
 const Pagination = (data) => {
 
-    const { pageLimit,totalPost,navigatePage} = data;
-    const [counter,setCounter] = useState(1)
-    console.log('val===='+totalPost);
-    const [totalPageBtn,setTotalPageBtn] = useState(Math.ceil(totalPost/pageLimit))
-    const [showPreviousBtn,setShowPreviousBtn] = useState(true)
-    const [showNextBtn,setShowNextBtn] = useState(false)
+    const { pageLimit, totalPost, navigatePage } = data;
+    const [counter, setCounter] = useState(1)
+    console.log('val====' + totalPost);
+    const totalPageBtn = Math.ceil(totalPost / pageLimit)
+    // const [showPreviousBtn,setShowPreviousBtn] = useState(true)
+    // const [showNextBtn,setShowNextBtn] = useState(false)
 
     const managePaging = (button) => {
-        if(button === "prev"){
-            if(counter == 1){
+        if (button === "prev") {
+            if (counter == 1) {
                 setCounter(1);
-                setShowPreviousBtn(true);
+                // setShowPreviousBtn(true);
             } else {
-                
-                if(counter == 2){
-                    setShowPreviousBtn(true);
+
+                if (counter == 2) {
+                    // setShowPreviousBtn(true);
                 } else {
-                    setShowPreviousBtn(false);
+                    // setShowPreviousBtn(false);
                 }
                 setCounter(counter - 1);
             }
-            setShowNextBtn(false);
+            // setShowNextBtn(false);
 
 
-        }else if(button === "next") {
-            if(counter == totalPageBtn){
+        } else if (button === "next") {
+            if (counter == totalPageBtn) {
                 setCounter(counter);
-                setShowNextBtn(true);
+                // setShowNextBtn(true);
             } else {
-                if(counter == totalPageBtn - 1){
-                    setShowNextBtn(true);
+                if (counter == totalPageBtn - 1) {
+                    // setShowNextBtn(true);
                 } else {
-                    setShowNextBtn(false);
+                    // setShowNextBtn(false);
                 }
                 setCounter(counter + 1);
             }
-            setShowPreviousBtn(false);
+            // setShowPreviousBtn(false);
 
         }
-        
+
     }
     useEffect(() => {
-        const result = counter*pageLimit;
-        navigatePage((parseInt(result) - parseInt(pageLimit)),result);
-        console.log("valueeee===="+totalPost);
-    },[counter])
+        const result = counter * pageLimit;
+        navigatePage((parseInt(result) - parseInt(pageLimit)), result);
+        debugger
+        console.log("valueeee====" + totalPost);
+    }, [counter])
 
     return (
 
         <div className="d-flex justify-content-center">
             <nav aria-label="Page navigation example">
                 <ul class="pagination">
-                    <li class="page-item"><a class="page-link" onClick={e => { managePaging("prev")}}>Previous</a></li>
+                    <li class="page-item"><a class="page-link" onClick={e => { managePaging("prev") }}>Previous</a></li>
                     {
-                        new Array(totalPageBtn).fill("").map((pageBtn,index) => (
-                            <li class={`page-item  ${index == counter-1 ? "active" : "" }`}><a class="page-link" onClick={(e) => setCounter(index+1)}>{index+1}</a></li>
+                        new Array(totalPageBtn).fill("").map((pageBtn, index) => (
+                            <li class={`page-item  ${index == counter - 1 ? "active" : ""}`}>
+                                <a class="page-link" onClick={(e) => setCounter(index + 1)}>{index + 1}</a>
+                            </li>
                         ))
                     }
-                    <li class="page-item"><a class="page-link" onClick={e => { managePaging("next")}}>Next</a></li>
+                    <li class="page-item"><a class="page-link" onClick={e => { managePaging("next") }}>Next</a></li>
                 </ul>
             </nav>
         </div>
-    // <div className="d-flex justify-content-between">
-    //     <nav aria-label="Page navigation example">
-    //         <ul class="pagination">
-    //             {/* <button className="btn btn-primary previous-btn {showPreviousBtn == true  ? 'hidden' : ''"  onClick={e => { managePaging("prev")}}>Previous</button> */}
-    //             <button className="btn btn-primary previous-btn"  disabled={showPreviousBtn} onClick={e => { managePaging("prev")}}>Previous</button>
-    //             {/* <button className="btn btn-primary previous-btn"  onClick={e => { managePaging("prev")}}>Previous</button> */}
-    //             <button className="btn btn-primary next-btn" disabled={showNextBtn} onClick={e => { managePaging("next")}}>Next</button>
-    //         </ul>
-    //     </nav>
-    // </div>
+        // <div className="d-flex justify-content-between">
+        //     <nav aria-label="Page navigation example">
+        //         <ul class="pagination">
+        //             {/* <button className="btn btn-primary previous-btn {showPreviousBtn == true  ? 'hidden' : ''"  onClick={e => { managePaging("prev")}}>Previous</button> */}
+        //             <button className="btn btn-primary previous-btn"  disabled={showPreviousBtn} onClick={e => { managePaging("prev")}}>Previous</button>
+        //             {/* <button className="btn btn-primary previous-btn"  onClick={e => { managePaging("prev")}}>Previous</button> */}
+        //             <button className="btn btn-primary next-btn" disabled={showNextBtn} onClick={e => { managePaging("next")}}>Next</button>
+        //         </ul>
+        //     </nav>
+        // </div>
     )
 }
 
--- src/Pages/Users/List.js
+++ src/Pages/Users/List.js
@@ -1,49 +1,71 @@
-import {React , useEffect, useState}  from "react";
-import  axios  from "axios";
-import { useHistory,Link } from "react-router-dom";
-import Pagination from "../../Layouts/Pagination";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+import { useHistory, Link } from "react-router-dom";
+// import Pagination from "../../Layouts/Pagination";
+import Pagination from '@material-ui/lab/Pagination';
 
 const List = () => {
 
-    const [User,setUser] = useState([]);
-    const [showNumber,setShowNumber] = useState(false);
-    const history = useHistory([]);
+    const pageLimit = 5
+    const [User, setUser] = useState([]);
+    // const [showNumber, setShowNumber] = useState(false);
+    // const history = useHistory([]);
+    const [page, setPage] = useState(1);
+    const [paging, setPaging] = useState({
+        start: pageLimit * page - pageLimit,
+        end: pageLimit * page
+    })
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPaging({
+            start: pageLimit * value - pageLimit,
+            end: pageLimit * value
+        })
+        setPage(value);
+    };
 
-    useEffect(()=>{ 
+    useEffect(() => {
         loadUsers();
-    },[]);
+    }, []);
 
-    const loadPagination = (pageLimit,totalNumber,navigatePage) => {
-        console.log("first==="+totalNumber);
-        //setTimeout(function(){ 
-            return (
-                <Pagination pageLimit={pageLimit} totalPost = {totalNumber} navigatePage={navigatePage}/>
-            ) 
-         //}, 1000)
-        
-    }
+    // const loadPagination = (pageLimit, totalNumber, navigatePage) => {
+    //     console.log("first===" + totalNumber);
+    //     //setTimeout(function(){ 
+    //     return (
+    //         <Pagination pageLimit={pageLimit} totalPost={totalNumber} navigatePage={navigatePage} />
+    //     )
+    //     //}, 1000)
+
+    // }
 
     const loadUsers = async (action = "") => {
-        const result = await axios.get('http://localhost:3002/user'); 
+        const result = await axios.get('http://localhost:3002/user');
         setUser(result.data.reverse());
-        
-        if(action == "delete"){
-            setShowNumber(true)
-
-            console.log('seconnd=='+parseInt(User.length));
-            console.log('seconnddd=='+parseInt(result.data.length));
-            console.log('seconnd=='+showNumber);
-        } else {
-            setShowNumber(false)
+        let CurrentPage = Math.ceil(result.data.length / pageLimit)
+        if (CurrentPage < page) {
+            setPage(CurrentPage)
+            setPaging({
+                start: pageLimit * CurrentPage - pageLimit,
+                end: pageLimit * CurrentPage
+            })
         }
-        
+
+        // if (action == "delete") {
+        //     setShowNumber(true)
+
+        //     console.log('seconnd==' + parseInt(User.length));
+        //     console.log('seconnddd==' + parseInt(result.data.length));
+        //     console.log('seconnd==' + showNumber);
+        // } else {
+        //     setShowNumber(false)
+        // }
+
     }
 
-    const searchUser = async(e) => {
+    const searchUser = async (e) => {
         console.log(e.target.value);
         let searchWord = e.target.value;
-        if(searchWord.length > 0){
-            const searchResults =  await axios.get('http://localhost:3002/user?q='+searchWord);
+        if (searchWord.length > 0) {
+            const searchResults = await axios.get('http://localhost:3002/user?q=' + searchWord);
             setUser(searchResults.data.reverse());
         } else {
             loadUsers();
@@ -53,38 +75,35 @@ const List = () => {
     const removeUser = async (id) => {
         await axios.delete(`http://localhost:3002/user/${id}`);
         const action = "delete";
-        history.push("/users");
+        loadUsers();
+        // navigatePage(1, 5)
+        // history.push("/users");
         //loadUsers(action);
     }
 
-    const[pageLimit, setPageLimit] = useState(5)
 
-    const[paging,setPaging] = useState({
-        start : 0,
-        end: pageLimit
-    })
 
-    const navigatePage = (start,end) =>{
-        setPaging({start : start,end : end})
-    }
+    // const navigatePage = (start, end) => {
+    //     setPaging({ start: start, end: end })
+    // }
 
-        // if (!this.state.response){
-        //     return <div>Loading...</div>
-        // }
-    
-    return ( 
+    // if (!this.state.response){
+    //     return <div>Loading...</div>
+    // }
+
+    return (
         <div className="container">
             <h2>User List</h2>
             <div className="row">
                 <div className="col-md-3">
                     <div className="pull-right">
-                        <Link className = "btn btn-primary" exact to="/add-user">Add User</Link>
+                        <Link className="btn btn-primary" exact to="/add-user">Add User</Link>
                     </div>
                 </div>
                 <div className="col-md-3" >
                     <div className="input-group">
                         <div className="form-outline">
-                            <input type="search" id="form1" className="form-control" placeholder="Search" onChange={(e)=> searchUser(e)}/>
+                            <input type="search" id="form1" className="form-control" placeholder="Search" onChange={(e) => searchUser(e)} />
                             {/* <label className="form-label" for="form1">Search</label> */}
                         </div>
                         {/* <button type="button" className="btn btn-primary">
@@ -95,43 +114,43 @@ const List = () => {
             </div>
             <table className="table table-hover">
                 <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    User.slice(paging.start,paging.end).map((value,index) => (
-                        <tr key={index}>
-                            <th scope="row">{index+1}</th>
-                            <td>{value.name}</td>
-                            <td>{value.email}</td>
-                            <td>{value.phone}</td>
-                            <td>
-                                <Link className = "btn btn-primary mr-2" to={`/view-user/${value.id}`}>View</Link>
-                                <Link className = "btn btn-outline-primary mr-2" to={`/edit-user/${value.id}`}>Edit</Link>
-                                <Link className = "btn btn-danger" onClick={(e) => removeUser(value.id)}>Delete</Link>
-                            </td>
-                        </tr>
-                    ))
-                }
-                {`"`+User.length+`"`}
-            </tbody>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        User.slice(paging.start, paging.end).map((value, index) => (
+                            <tr key={index}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{value.name}</td>
+                                <td>{value.email}</td>
+                                <td>{value.phone}</td>
+                                <td>
+                                    <Link className="btn btn-primary mr-2" to={`/view-user/${value.id}`}>View</Link>
+                                    <Link className="btn btn-outline-primary mr-2" to={`/edit-user/${value.id}`}>Edit</Link>
+                                    <Link className="btn btn-danger" onClick={(e) => removeUser(value.id)}>Delete</Link>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
             </table>
-            
+            <Pagination count={Math.ceil(User.length / pageLimit)} page={page} onChange={handleChange} />
+
             {
-                showNumber ? loadPagination(pageLimit,parseInt(User.length-1),navigatePage) : (User.length > 0 ?  loadPagination(pageLimit,parseInt(User.length),navigatePage): <div> Loading ... </div> )
+                // loadPagination(pageLimit, parseInt(User.length), navigatePage)
+                // (User.length > 0 ? loadPagination(pageLimit, parseInt(User.length), navigatePage) : <div> Loading ... </div>)
                 // showNumber ? loadPagination(pageLimit,parseInt(User.length),navigatePage) : (User.length > 0 ?  loadPagination(pageLimit,parseInt(User.length),navigatePage) : <div> Loading ... </div> )
-            
             }
         </div>
     );
 
 
-    
+
 }
 export default List; 
