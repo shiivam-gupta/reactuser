b40d06b2aff1849ab316fe8728759610f9dc61d1
najmussaher <najmussaher.siddiqui@appinventiv.com>
2021-04-07 15:44:41 +0530

    changes for route

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -2,9 +2,16 @@ import { React } from "react";
 import { Route, Redirect } from "react-router-dom";
 import Navbar from "../Layouts/Navbar";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+const PrivateRoute = ({ component: Component }) => {
     const token = localStorage.getItem("token");
-    return token ? (<Route {...rest} render={(props) => <><Navbar /><Component {...rest} {...props} /></>} />) : (<Redirect to="/login" />);
+    return token ?
+        // (<Route {...rest} render={(props) => <><Navbar /><Component {...rest} {...props} /></>} />) : 
+        (<Route>
+            <Navbar></Navbar>
+            <Component />
+        </Route>) :
+        (<Redirect to="/login" />);
 
 }
 
